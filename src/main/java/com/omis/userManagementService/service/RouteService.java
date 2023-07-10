5f330a18ec7865a5ab92fb3f9ed8f5491309bb9c
hsdha <hsdha@ou.ac.lk>
2023-07-10 09:02:38 +0530

    route management changes 2023.07.10

--- src/main/java/com/omis/userManagementService/service/RouteService.java
+++ src/main/java/com/omis/userManagementService/service/RouteService.java
@@ -1,11 +1,16 @@
 package com.omis.userManagementService.service;
 
 import com.omis.userManagementService.models.Route;
+import com.omis.userManagementService.payload.response.RouteRequest;
 import com.omis.userManagementService.payload.response.RouteResponse;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface RouteService {
     List<Route> getAllRoutes();
     List<RouteResponse> getAllRouteResponse();
+    String addNewRoute(RouteRequest routeRequest);
+    String updateRoute(Route route);
+    String archiveRoute(Long id);
 }
